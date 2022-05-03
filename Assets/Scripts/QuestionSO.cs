61da07e519823e763ea59165ac0540f44e157763
Aneesh Babu <73874288+Aneeshbabu2106@users.noreply.github.com>
2022-05-03 18:43:51 +0530

    adding question to UI

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -6,5 +6,22 @@ using UnityEngine;
 public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
-    [SerializeField] string Questions = "Enter new Questions";
+    [SerializeField] string questions = "Enter new Questions";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswer;
+
+    public string GetQuestions()
+    {
+        return questions;
+    }
+
+    public string GetAnswers(int index)
+    {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswer()
+    {
+        return correctAnswer;
+    }
 }
